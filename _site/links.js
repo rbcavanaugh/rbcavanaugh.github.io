0149195597f0ea8d91ca08a69dbed204a6055281
Rob Cavanaugh <rob.cavanaugh@pitt.edu>
2020-12-28 10:08:00 -0500

    new cv link and titles

--- _site/links.js
+++ _site/links.js
@@ -3,10 +3,3 @@
     if (/^(https?:)?\/\//.test(link.getAttribute('href'))) link.target = '_blank'; link.rel = 'noreferrer';
   }
 })();
-
-
-(function() {
-  for (const link of document.getElementsByTagName('a')) {
-    if (/\.pdf$/.test(link.getAttribute('href'))) link.target = '_blank'; link.rel = 'noreferrer';
-  }
-})();
