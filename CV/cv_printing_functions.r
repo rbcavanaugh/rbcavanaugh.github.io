c38566b2bc62b09f519ecc31d8a5b348564b6edf
Rob Cavanaugh <rob.cavanaugh@pitt.edu>
2021-04-18 20:01:11 -0400

    try again

--- CV/cv_printing_functions.r
+++ CV/cv_printing_functions.r
@@ -17,14 +17,14 @@
 create_CV_object <-  function(data_location,
                               pdf_mode = FALSE,
                               sheet_is_publicly_readable = TRUE) {
-
+  
   cv <- list(
     pdf_mode = pdf_mode,
     links = c()
   )
-
+  
   is_google_sheets_location <- stringr::str_detect(data_location, "docs\\.google\\.com")
-
+  
   if(is_google_sheets_location){
     if(sheet_is_publicly_readable){
       # This tells google sheets to not try and authenticate. Note that this will only
@@ -36,7 +36,7 @@ create_CV_object <-  function(data_location,
       # designate project-specific cache so we can render Rmd without problems
       options(gargle_oauth_cache = ".secrets")
     }
-
+    
     read_gsheet <- function(sheet_id){
       googlesheets4::read_sheet(data_location, sheet = sheet_id, skip = 1, col_types = "c")
     }
@@ -51,24 +51,24 @@ create_CV_object <-  function(data_location,
     cv$text_blocks  <- readr::read_csv(paste0(data_location, "text_blocks.csv"), skip = 1)
     cv$contact_info <- readr::read_csv(paste0(data_location, "contact_info.csv"), skip = 1)
   }
-
-
+  
+  
   extract_year <- function(dates){
     date_year <- stringr::str_extract(dates, "(20|19)[0-9]{2}")
     date_year[is.na(date_year)] <- lubridate::year(lubridate::ymd(Sys.Date())) + 10
-
+    
     date_year
   }
-
+  
   parse_dates <- function(dates){
-
+    
     date_month <- stringr::str_extract(dates, "(\\w+|\\d+)(?=(\\s|\\/|-)(20|19)[0-9]{2})")
     date_month[is.na(date_month)] <- "1"
-
+    
     paste("1", date_month, extract_year(dates), sep = "-") %>%
       lubridate::dmy()
   }
-
+  
   # Clean up entries dataframe to format we need it for printing
   cv$entries_data %<>%
     tidyr::unite(
@@ -96,7 +96,7 @@ create_CV_object <-  function(data_location,
     ) %>%
     dplyr::arrange(desc(parse_dates(end))) %>%
     dplyr::mutate_all(~ ifelse(is.na(.), 'N/A', .))
-
+  
   cv
 }
 
@@ -106,27 +106,27 @@ sanitize_links <- function(cv, text){
   if(cv$pdf_mode){
     link_titles <- stringr::str_extract_all(text, '(?<=\\[).+?(?=\\])')[[1]]
     link_destinations <- stringr::str_extract_all(text, '(?<=\\().+?(?=\\))')[[1]]
-
+    
     n_links <- length(cv$links)
     n_new_links <- length(link_titles)
-
+    
     if(n_new_links > 0){
       # add links to links array
       cv$links <- c(cv$links, link_destinations)
-
+      
       # Build map of link destination to superscript
       link_superscript_mappings <- purrr::set_names(
         paste0("<sup>", (1:n_new_links) + n_links, "</sup>"),
         paste0("(", link_destinations, ")")
       )
-
+      
       # Replace the link destination and remove square brackets for title
       text <- text %>%
         stringr::str_replace_all(stringr::fixed(link_superscript_mappings)) %>%
         stringr::str_replace_all('\\[(.+?)\\]', "\\1")
     }
   }
-
+  
   list(cv = cv, text = text)
 }
 
@@ -135,7 +135,7 @@ sanitize_links <- function(cv, text){
 #' @param section_id ID of the entries section to be printed as encoded by the `section` column of the `entries` table
 #' @param glue_template controls formatting. optiosn are default,  other, grant, citation
 print_section <- function(cv, section_id, glue_template = "default"){
-
+  
   if(glue_template == "default"){
     glue_template <- "
 ### {title}
@@ -182,9 +182,9 @@ print_section <- function(cv, section_id, glue_template = "default"){
 {timeline}
 \n\n\n"
   }
-
+  
   section_data <- dplyr::filter(cv$entries_data, section == section_id)
-
+  
   # Take entire entries data frame and removes the links in descending order
   # so links for the same position are right next to each other in number.
   for(i in 1:nrow(section_data)){
@@ -194,9 +194,9 @@ print_section <- function(cv, section_id, glue_template = "default"){
       cv <- strip_res$cv
     }
   }
-
+  
   print(glue::glue_data(section_data, glue_template))
-
+  
   invisible(strip_res$cv)
 }
 
@@ -207,11 +207,11 @@ print_section <- function(cv, section_id, glue_template = "default"){
 print_text_block <- function(cv, label){
   text_block <- dplyr::filter(cv$text_blocks, loc == label) %>%
     dplyr::pull(text)
-
+  
   strip_res <- sanitize_links(cv, text_block)
-
+  
   cat(strip_res$text)
-
+  
   invisible(strip_res$cv)
 }
 
@@ -236,7 +236,7 @@ print_text_block <- function(cv, label){
 #' @description Construct a bar chart of skills
 #' @param out_of The relative maximum for skills. Used to set what a fully filled in skill bar is.
 print_skill_bars <- function(cv, out_of = 5, bar_color = "#969696", bar_background = "#d9d9d9", glue_template = "default"){
-
+  
   if(glue_template == "default"){
     glue_template <- "
 <div
@@ -250,7 +250,7 @@ print_skill_bars <- function(cv, out_of = 5, bar_color = "#969696", bar_backgrou
     dplyr::mutate(width_percent = round(100*as.numeric(level)/out_of)) %>%
     glue::glue_data(glue_template) %>%
     print()
-
+  
   invisible(cv)
 }
 
@@ -268,23 +268,23 @@ Links {data-icon=link}
 
 
 ")
-
+    
     purrr::walk2(cv$links, 1:n_links, function(link, index) {
       print(glue::glue('{index}. {link}'))
     })
   }
-
+  
   invisible(cv)
 }
 
 
-  
+
 #' @description Contact information section with icons
 print_contact_info <- function(cv){
   glue::glue_data(
     cv$contact_info,
     "- <i class='{icon}'></i> {contact}"
   ) %>% print()
-
+  
   invisible(cv)
 }
--- CV/render_cv.r
+++ CV/render_cv.r
@@ -4,19 +4,12 @@
 # this script to cache a version of the CV_Printer class with data already
 # loaded and load the cached version in the .Rmd instead of re-fetching it twice
 # for the HTML and PDF rendering. This exercise is left to the reader.
-
 library(here)
 # Knit the HTML version
 rmarkdown::render(here("CV", "cv.rmd"),
                   #params = list(pdf_mode = FALSE),
-                  output_file = here("_site", "cv.html"))
-
-# Knit the PDF version to temporary html location
-tmp_html_cv_loc <- fs::file_temp(ext = ".html")
-rmarkdown::render(here("CV", "cv.rmd"),
-                  params = list(pdf_mode = TRUE),
-                  output_file = tmp_html_cv_loc)
+                  output_file = "~/Documents/github-repos/personal-website-distill/_site/cv.html")
 
 # Convert to PDF using Pagedown
-pagedown::chrome_print(input = tmp_html_cv_loc,
+pagedown::chrome_print(input = "~/Documents/github-repos/personal-website-distill/_site/cv.html",
                        output = here("CV", "cv.pdf"))
