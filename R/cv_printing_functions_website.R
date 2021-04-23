bf6c859581b50cbcfb21353e3faae6f3cd346b9a
Rob Cavanaugh <rob.cavanaugh@pitt.edu>
2021-04-23 11:03:37 -0400

    margin not padding

--- R/cv_printing_functions_website.R
+++ R/cv_printing_functions_website.R
@@ -151,7 +151,7 @@ print_section <- function(cv, section_id, glue_template = "article"){
  </a>
  </div>
  <div class="flex-right">
-<p style="padding:0px;">{title}</p>
+<p style="margin:0px">{title}</p>
 </div>
 </div>
 \n\n\n'
