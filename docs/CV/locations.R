0f3c60488f94425a8ea4cec82910f113e9d9fb3a
Rob Cavanaugh <46727239+rbcavanaugh@users.noreply.github.com>
2022-10-26 07:22:55 -0400

    aoa

---
+++ docs/CV/locations.R
@@ -0,0 +1,7 @@
+datadrivencv::use_datadriven_cv(
+  full_name = "Robert Cavanaugh",
+  data_location = "https://docs.google.com/spreadsheets/d/1B2gc55CwWn0QidoLk3aqWXFaKGFXqekKV94LVyY7Gug/edit?usp=sharing",
+  pdf_location = "https://github.com/rbcavanaugh/personal-website-distill/raw/master/cv.pdf",
+  html_location = "https://github.com/rbcavanaugh/personal-website-distill/raw/master/_site/cv.html",
+  source_location = "/Users/robcavanaugh/Documents/github-repos/personal-website-distill"
+)
